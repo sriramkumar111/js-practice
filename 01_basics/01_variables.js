1f9946359400ff0431cd30e975e49c18dc5ed411
sriramkumar111 <136987311+sriramkumar111@users.noreply.github.com>
2024-09-08 12:03:24 +0000

    conversion and operation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 23456
+var accountPassword = "ram"
+let accountEmail ="ram@gmail.com"
+
+accountEmail ="sri@gmail.com"
+accountMoney ="45667"
+let account
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountMoney, account])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+let state
+console.log(typeof state)
